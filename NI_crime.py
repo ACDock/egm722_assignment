fe953d1f9f95e6c98dc6c80ddfe2276ccfc4e268
ACDock <127091143+ACDock@users.noreply.github.com>
2023-05-01 17:19:49 +0100

    Update script to change colour of town labels
    Also updated comments to lines which need changing when plotting individual crime types

--- NI_crime.py
+++ NI_crime.py
@@ -9,6 +9,7 @@ import matplotlib.patches as mpatches
 # Load data from files and
 # reproject data files into same crs as NI outline
 outline = gpd.read_file('data_files/NI_outline.shp')
+print(outline.crs)
 lgd = gpd.read_file('data_files/LGD.shp').to_crs(epsg=32629)
 crimes = gpd.read_file('data_files/NI_crimes.shp').to_crs(epsg=32629)
 towns = gpd.read_file('data_files/Towns.shp').to_crs(epsg=32629)
@@ -52,6 +53,8 @@ def generate_handles(labels, colors, edge='k', alpha=1):
     return handles
 
 # Create a scale bar for map
+# adapted this question: https://stackoverflow.com/q/32333870
+# answered by SO user Siyh: https://stackoverflow.com/a/35705477
 def scale_bar(ax, location=(0.92, 0.95)):
     """Generate a scale bar for map using Cartopy.
 
@@ -70,7 +73,7 @@ def scale_bar(ax, location=(0.92, 0.95)):
     ax.text(sbx-12500, sby-4500, '10 km', transform=ax.projection, fontsize=8)
     ax.text(sbx-24500, sby-4500, '0 km', transform=ax.projection, fontsize=8)
 
-# Create a figure of size 10x10 inches representing the page size in inches
+# Create a figure of size 10x10 representing the page size in inches
 myFig = plt.figure(figsize=(10, 10))
 
 # Create a UTM reference system to transform the data
@@ -81,6 +84,7 @@ myCRS = ccrs.UTM(29)
 ax = plt.axes(projection=myCRS)
 
 # Add title to the map
+# The ext inside the quotation marks can be changed if alternative data used
 ax.set_title('Crime incidents in Northern Ireland (Dec 17 - Nov 18)', fontsize=14, fontweight='bold')
 
 # Add the outline of Northern Ireland using cartopy's ShapelyFeature
@@ -111,23 +115,23 @@ for ii, name in enumerate(lgd_names):
     ax.add_feature(feat)
 
 # Add point data of NI crimes, towns, and cities to the map
-crimes_handle = ax.plot(crimes.geometry.x, crimes.geometry.y, 'o', color='black', ms=3, transform=myCRS)
+crimes_handle = ax.plot(crimes.geometry.x, crimes.geometry.y, 'o', color='black', ms=3, transform=myCRS) #Comment out this line if plotting individual crime types
 town_handle = ax.plot(towns.loc[towns['STATUS'] == 'Town'].geometry.x, towns.loc[towns['STATUS'] == 'Town'].geometry.y,
                       's', color='orange', ms=4, transform=myCRS)
 city_handle = ax.plot(towns.loc[towns['STATUS'] == 'City'].geometry.x, towns.loc[towns['STATUS'] == 'City'].geometry.y,
                       'o', color='r', ms=6, transform=myCRS)
-#crimetype_handle = ax.plot(crimes.loc[crimes['Crime_type'] == 'Anti-social behaviour'].geometry.x,
-#                            crimes.loc[crimes['Crime_type'] == 'Anti-social behaviour'].geometry.y, 'o', color='k',
+#crimetype_handle = ax.plot(crimes.loc[crimes['Crime_type'] == 'Anti-Social Behaviour'].geometry.x,
+#                            crimes.loc[crimes['Crime_type'] == 'Anti-Social Behaviour'].geometry.y, 'o', color='k',
 #                            ms=4, transform=myCRS)
 
 # Generate a list of handles for the LGD dataset
 lgd_handle = generate_handles(lgd.LGDNAME.unique(), lgd_colors, alpha=0.25)
 
 # Make a list of handles and labels corresponding to the objects required in legend
-handles = lgd_handle + crimes_handle + town_handle + city_handle
-labels = lgd_names + ['Crimes', 'Towns', 'Cities']
+handles = lgd_handle + crimes_handle + town_handle + city_handle #Comment out this line if plotting individual crime types
+labels = lgd_names + ['Crimes', 'Towns', 'Cities'] #Comment out this line if plotting individual crime types
 #handles = lgd_handle + crimetype_handle + town_handle + city_handle
-#labels = lgd_names + ['Anti-social behaviour', 'Towns', 'Cities']
+#labels = lgd_names + ['Anti-Social Behaviour', 'Towns', 'Cities']
 
 # Add legend to map in upper left corner
 leg = ax.legend(handles, labels, title='Legend', title_fontsize=10,
@@ -144,7 +148,7 @@ gridlines.bottom_labels = False
 # Add the text labels for the towns and cities
 for ind, row in towns.iterrows():
         x, y = row.geometry.x, row.geometry.y
-        ax.text(x, y, row['TOWN_NAME'].title(), color='blue', fontsize=10, fontweight='bold', transform=myCRS)
+        ax.text(x, y, row['TOWN_NAME'].title(), color='magenta', fontsize=10, fontweight='bold', transform=myCRS)
 
 # Add the scale bar to the map
 scale_bar(ax)
